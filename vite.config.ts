8861958f1dc02addd231f48deef7824104ba2bec
Rick Franze <rickfranze@gmail.com>
2026-07-06 23:02:30 -0600

    build(01-01): scaffold project configuration and dependencies

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+/// <reference types="vitest" />
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  test: {
+    environment: 'node',
+    globals: true,
+    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+  },
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+});
